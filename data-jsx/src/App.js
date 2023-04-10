6e577927d1aa867e1c7ceb37584f46afbea3a8c2
Ballsky22 <ballskyteam22@gmail.com>
2023-04-10 21:35:19 +0700

    Looping Data in JSX

--- data-jsx/src/App.js
+++ data-jsx/src/App.js
@@ -1,9 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
+
+const gamepadData = [
+  { name: "Dualshock 3", price: 300000, discount: 30 },
+  { name: "Dualshock 4", price: 800000, discount: 40 },
+  { name: "Dualshock 5", price: 1500000, discount: 50 },
+  { name: "Dualshock 6", price: 1700000, discount: 60 },
+];
 
 function Product({ price, name, discount = 0 }) {
   return (
-    <div>
+    <div className="App">
       <h2>{name}</h2>
       <p>
         <s>Rp. {price}</s> ({discount}%)
@@ -20,12 +26,26 @@ function Product({ price, name, discount = 0 }) {
 
 function App() {
   return (
-    <div className="App">
-      <Product name="DualShock 4" price="700000" discount="50" />
-      <Product name="DualShock 5" price="1500000" discount="30" />
-      <Product name="DualShock 6" price="1800000" discount="20" />
+    <div>
+      {gamepadData.map((stik, id) => (
+        <Product
+          key={id}
+          name={stik.name}
+          price={stik.price}
+          discount={stik.discount}
+        ></Product>
+      ))}
     </div>
   );
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <Product name="DualShock 4" price="700000" discount="50" />
+//       <Product name="DualShock 5" price="1500000" discount="30" />
+//       <Product name="DualShock 6" price="1800000" discount="20" />
+//     </div>
+//   );
+// }
 
 export default App;
